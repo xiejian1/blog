43f064cfb2b92f15e1f8a67ade1b0b90a1fd62f6
xiejian1 <1270392489@qq.com>
2018-12-25 22:26:22 +0800

    使用通用视图对文章视图进行修改

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import url
 from django.conf.urls.static import static
 
-from blog.views import index,detail,archives,category
+from blog.views import archives,IndexView,CategoryView,DetailPostView
 from blogproject import settings
 app_name = 'blog'
 urlpatterns = [
-    url(r'index/$',index,name='list'),
-    url(r'post/(?P<pk>\d+)/$',detail,name='detail'),
+    # url(r'index/$',index,name='list'),
+    url(r'index/$',IndexView.as_view(),name='list'),
+    # url(r'post/(?P<pk>\d+)/$',detail,name='detail'),
+    url(r'post/(?P<pk>\d+)/$',DetailPostView.as_view(),name='detail'),
     url(r'archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$',archives,name='archives'),
-    url(r'category/(?P<pk>\d+)/$',category,name='category'),
+    # url(r'category/(?P<pk>\d+)/$',category,name='category'),
+    url(r'category/(?P<pk>\d+)/$',CategoryView.as_view(),name='category'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,5 +1,7 @@
 import markdown as markdown
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DetailView
+
 from blog.models import Post, Category
 
 
@@ -11,6 +13,12 @@ def index(request):
     # post_list = Post.objects.all().order_by('-created_time')
     post_list = Post.objects.all()
     return render(request,'blog/index.html',{'post_list':post_list})
+
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
 
 def detail(request,pk):
     post = get_object_or_404(Post, pk=pk)
@@ -38,3 +46,51 @@ def category(request, pk):
     cate = get_object_or_404(Category, pk=pk)
     post_list = Post.objects.filter(category=cate).order_by('-created_time')
     return render(request, 'blog/index.html', context={'post_list': post_list})
+class CategoryView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    def get_queryset(self):
+        cate = get_object_or_404(Category,pk=self.kwargs.get('pk'))
+        return super(CategoryView,self).get_queryset().filter(category=cate)
+
+class DetailPostView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        # 覆写 get 方法的目的是因为每当文章被访问一次，就得将文章阅读量 +1
+        # get 方法返回的是一个 HttpResponse 实例
+        # 之所以需要先调用父类的 get 方法，是因为只有当 get 方法被调用后，
+        # 才有 self.object 属性，其值为 Post 模型实例，即被访问的文章 post
+        response = super(DetailPostView, self).get(request, *args, **kwargs)
+        # 将文章阅读量 +1
+        # 注意 self.object 的值就是被访问的文章 post
+        self.object.increase_view()
+        # 视图必须返回一个 HttpResponse 对象
+        return response
+
+    def get_object(self, queryset=None):
+        # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+        post = super(DetailPostView, self).get_object(queryset=None)
+        post.body = markdown.markdown(post.body,extensions = [
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        'markdown.extensions.toc',])
+        return post
+
+    def get_context_data(self, **kwargs):
+        # 覆写 get_context_data 的目的是因为除了将 post 传递给模板外（DetailView 已经帮我们完成），
+        # 还要把评论表单、post 下的评论列表传递给模板。
+        context = super(DetailPostView, self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form': form,
+            'comment_list': comment_list,
+        })
+        return context
+
+
+
